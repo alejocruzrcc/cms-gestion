76c86ecd8f6a42d4c8f7334242028bb7fda3df00
Luis Felipe Holguin <luis@farleyetc.com>
2016-11-29 17:29:58 -0500

    clone activity

--- js/main.js
+++ js/main.js
@@ -6,11 +6,21 @@
         
     });
 
+    $( "#sortable2" ).sortable({
+    });
+    $( ".ui-state-default" ).draggable({
+     connectToSortable: "#sortable2",
+     helper: "clone",
+     revert: "invalid"
+    });
+    $( "ul, li" ).disableSelection();
+   
 
+/*
     $( "#sortable1, #sortable2" ).sortable({
       connectWith: ".connectedSortable"
-    }).disableSelection();
-    $( ".connectedSortable" ).sortable( "option", "helper", "clone" );
+    });
+   */
 
     });
 
